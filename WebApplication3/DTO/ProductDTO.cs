93364d17659c4627550df2d1e66843140a5ca44a
xr7a <ivandesyatov3@gmail.com>
2024-03-09 20:15:29 +0300

    product model

---
+++ WebApplication3/DTO/ProductDTO.cs
@@ -0,0 +1,9 @@
+namespace WebApplication3.DTO;
+
+public class ProductDTO
+{
+    public string name { get; set; }
+    public string description { get; set; }
+    public string category { get; set; }
+    public float price { get; set; }
+}
